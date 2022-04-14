5f1e4f3a14a476558dc65133dc836336a62b9ba3
Maria Jose Castellano <majo_cas26@hotmail.com>
2022-04-14 14:28:11 +0200

    Iteration 1,2,3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// Second iteration: Going into business
+// Third iteration: Extra hot
+// Fourth iteration: Making money
+// Fifth iteration Running out
+
 function App() {
+  const [commande, setCommande] = useState(null)
+  const [error, setError] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const handleMakingDrinks = (drink, wantHotDrink, money, sugar) => {
+    handleOrderTaking(drink, wantHotDrink, sugar)
+    handleDrinkPayment(money, drink)
+  }
+  
+  const handleOrderTaking = (drink, wantHotDrink, sugar) => {
+    onPickDrink(drink, wantHotDrink);
+    onPickSugarAndStick(sugar, drink);
+  }
+
+  const onPickDrink = (value, wantHotDrink) => {
+    let drinkType
+    switch (value) {
+      case 'Thé':
+        drinkType = 'T'
+        break;
+      case 'Chocolat':
+        drinkType = 'H'
+        break;
+      case 'Café':
+        drinkType = 'C'
+        break;
+      case 'Orange':
+        drinkType = 'O'
+        break;
+      default:
+        setError("Cette boisson n'est pas disponible")
+        break;
+    }
+    drinkType = onPickHotDrink(wantHotDrink, drinkType)
+    setCommande(drinkType)
+  }
+
+  const onPickHotDrink = (wantHotDrink, drinkType) => {
+    return drinkType !== 'O' && wantHotDrink ? drinkType.concat('h') : drinkType
+  }
+
+  const onPickSugarAndStick = (sugarQuantity, drink) => {
+    let orderInformation;
+    if (sugarQuantity !== 0 && !drink.includes('Orange')) {
+      orderInformation = `:${sugarQuantity}:0`
+    } else {
+      orderInformation = '::'
+    }
+    setCommande((prevCommande) => prevCommande = prevCommande.concat(orderInformation))
+  }
+
+  const handleDrinkPayment = (givenMoney, drink) => {
+    let isMinimumAmount;
+    switch (drink) {
+      case 'Thé':
+        isMinimumAmount = givenMoney >= 0.4
+        console.log(isMinimumAmount)
+        break;
+      case 'Chocolat':
+        isMinimumAmount = givenMoney >= 0.5
+        break;
+      case 'Café':
+        isMinimumAmount = givenMoney >= 0.6   
+        break;
+      case 'Orange':
+        isMinimumAmount = givenMoney >= 0.6   
+        break;
+      default:
+        setError("Cette boisson n'est pas disponible")
+        break;
+    }
+    if (!isMinimumAmount) {
+      setMessage("M: Il n'a y pas assez argent pour la commande")
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleMakingDrinks('Orange', true, 0.6, 2)}>Make drink</button>
+      {!message ? (
+        <div>{commande}</div>
+      ) : (
+        <div>{message}</div>
+      )}
     </div>
   );
 }
